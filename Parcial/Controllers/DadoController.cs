5081e924a6904d5126b4c3aede9a448770f15d60
DEAA243 <davidazucena243@gmail.com>
2025-02-28 21:29:29 -0600

    Agregar archivos de proyecto.

---
+++ Parcial/Controllers/DadoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial.Models;
+
+namespace Parcial.Controllers
+{
+    public class DadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var dado = new Dado();
+            int resultado = dado.Lanzar();
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+    }
+}
---
+++ Parcial/Models/Dado.cs
@@ -0,0 +1,12 @@
+﻿namespace Parcial.Models
+{
+    public class Dado
+    {
+        private static readonly Random aleatorio = new Random();
+
+        public int Lanzar()
+        {
+            return aleatorio.Next(1, 7);
+        }
+    }
+}
